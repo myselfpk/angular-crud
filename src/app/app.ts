7552359fdcccc6b6de1b924595f457a7843e3752
Pushpendra Singh <62493522+myselfpk@users.noreply.github.com>
2026-06-02 11:48:22 +0530

    first chnage for name

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-curd');
+  protected readonly title = signal('angular-crud');
 }
